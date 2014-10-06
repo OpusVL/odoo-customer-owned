123b2511777ba3248ece62ab57f06d0b6e06e8b3
Nick Booker <nick.booker@opusvl.com>
2014-10-06 15:18:36 +0100

    Update copyright notices.

--- product_customer_owned/customer_owned.py
+++ product_customer_owned/customer_owned.py
@@ -3,7 +3,7 @@
 ##############################################################################
 #
 # Customer Owned Products
-# Copyright (C) 2014 OpusVL (<http://opusvl.com>)
+# Copyright (C) 2014 OpusVL
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -12,11 +12,18 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# If you require assistance, support, or further development of this
+# software, please contact OpusVL using the details below:
+#
+# Telephone: +44 (0)1788 298 410
+# Email: community@opusvl.com
+# Web: http://opusvl.com
 #
 ##############################################################################
 
